7fb0f68a85944e8de7853105eca5683c02156811
Romik <makavanaromik1214@gmail.com>
2024-09-28 20:09:52 +0530

    configure ai with user profile ai key

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,28 @@
 import { AiToneOption, LanguageOption } from "@/components/types";
 
 export const DOCUMENT_COLLECTION = "docs";
+export const USER_COLLECTION = "users";
+
+export const AI_MODEL_GPT = "gpt-4o";
+export const AI_MODEL_GEMINI = "gemini-1.5-pro";
+export const AI_MODEL_MISTRAL = "mistral-large";
+export const AI_MODEL_CLAUDE = "claude-3-5-sonnet";
+export const AI_MODEL_LLAMA = "llama-v3p1-405b";
+
+export const AI_MODELS_LIST = [
+  AI_MODEL_GEMINI,
+  AI_MODEL_GPT,
+  AI_MODEL_MISTRAL,
+  AI_MODEL_CLAUDE,
+  AI_MODEL_LLAMA
+];
+
+export const DEFAULT_AI_MODEL = AI_MODEL_GPT;
+
+export const RESPONSE_CODE = {
+  api_key_not_found: "api_key_not_found",
+  api_key_not_set: "api_key_not_set"
+}
 
 export const languages: LanguageOption[] = [
   { name: 'arabic', label: 'Arabic', value: 'ar' as LanguageOption['value'] },
