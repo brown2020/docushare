327b8275a33e8c54ead1ceb16173e9a5a9a5b36d
Steve Brown <brown2020@gmail.com>
2024-10-28 20:40:09 -0700

    remove hardcoded name

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,7 +5,7 @@ import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import Footer from "./Footer";
-import logo from "@/assets/svg/logo.svg"
+import logo from "@/assets/svg/logo.svg";
 
 export default function Home() {
   const uid = useAuthStore((state) => state.uid);
@@ -18,21 +18,25 @@ export default function Home() {
       <SignedIn>
         <div className="flex items-center justify-center h-full gap-[100px]">
           <div className="flex flex-col gap-[30px] bg-white shadow-pop-up-shadow rounded-2xl p-[30px] max-w-[616px] w-full">
-            <h2 className="text-center font-medium  text-[26px]">Docushare AI Demo</h2>
+            <h2 className="text-center font-medium  text-[26px]">
+              Docushare AI Demo
+            </h2>
             <div className="flex flex-col items-center gap-5">
-                <div className="flex flex-col items-center mb-[10px]">
-                  <div className="overflow-hidden w-20 h-20 rounded-full">
-                    {photoUrl && (
-                      <Image
-                        src={photoUrl}
-                        width={80}
-                        height={80}
-                        alt={"user"}
-                        priority
-                      />
-                    )}
-                  </div>
-                <div className="text-center mt-[10px] text-[22px]">{fullName} Romik M </div>
+              <div className="flex flex-col items-center mb-[10px]">
+                <div className="overflow-hidden w-20 h-20 rounded-full">
+                  {photoUrl && (
+                    <Image
+                      src={photoUrl}
+                      width={80}
+                      height={80}
+                      alt={"user"}
+                      priority
+                    />
+                  )}
+                </div>
+                <div className="text-center mt-[10px] text-[22px]">
+                  {fullName}
+                </div>
               </div>
 
               <div className="w-full">
@@ -69,7 +73,9 @@ export default function Home() {
           </div>
           <div className="flex flex-col gap-5 bg-white shadow-pop-up-shadow rounded-2xl px-8 py-[50px] max-w-[616px] w-full">
             <div className="flex flex-col items-center">
-              <h2 className="font-medium text-[32px]">Welcome to the Docushare AI Demo!</h2>
+              <h2 className="font-medium text-[32px]">
+                Welcome to the Docushare AI Demo!
+              </h2>
               <div className="text-lg text-richBlack font-medium text-center mt-5 px-9">
                 This demo showcases the capabilities of the TipTap as an editor,
                 Firebase as a realtime database, and collaborative document
